0807a273e65fbe9d3f09e1d4ff931487e38ec007
atribolt <atribolt@gmail.com>
2020-06-20 08:54:03 +0300

    Fix low speed random nubers for loops

--- Math/Random.h
+++ Math/Random.h
@@ -6,13 +6,15 @@ namespace Math {
       std::default_random_engine dre;
 
    public:
+      Random() { ReSeed(); }
+
       void ReSeed() { dre.seed(std::random_device()()); }
 
       template<class T>
          auto Pick(T min, T max) {
             static_assert(std::is_fundamental_v<T>, "This function do work whith fundamental type only");
 
-            ReSeed();
+            //ReSeed(); // this wery low speed for fast call in loops
             T res{};
 
             if constexpr (std::is_floating_point_v<T>) {
