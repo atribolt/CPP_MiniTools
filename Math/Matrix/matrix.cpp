e65acbd6e546528388d58bc2ea50fa9800c80455
atribolt <atribolt@gmail.com>
2021-06-29 13:46:58 +0300

    Доработка, рефакторинг
    - разделил работу с шаблонным классом матриц и обычным динамическим
    - удалил лишнее, привел код к общему виду

--- Math/Matrix/matrix.cpp
+++ Math/Matrix/matrix.cpp
@@ -1,284 +1,236 @@
-﻿//#include "matrix.h"
-//
-//ostream& operator<<(ostream& os, const Matrix& m) {
-//  Elem e(0, 0);
-//  for(e.first = 0; e.first < m.Row(); ++e.first) {
-//    for(e.second = 0; e.second < m.Col(); ++e.second)
-//      os << m[e] << '\t';
-//    os << '\n';
-//  }
-//  return os;
-//}
-//Matrix GetMatrix () {
-//  size_t R, C;
-//  
-//  cout << "Размер матрицы (строк столбцов) через пробел: "; cin >> R >> C;
-//  cout << "Введите матрицу построчно:\n";
-//  
-//  Matrix m(R, C);
-//  string line, buf;
-//  stringstream ss;
-//  
-//  for(size_t e = 0; e < m.Size() && cin >> line; ++e) {
-//    m[e] = atof(line.data());
-//  }
-//  cin.clear();
-//  return m;
-//}
-//  
-//Matrix::Matrix(size_t row, size_t col) noexcept {
-//  this->Alloc(row, col); 
-//}
-//Matrix::Matrix(const Matrix& m) {
-//  this->Alloc(m.Row(), m.Col());
-//  this->Copy(m.data, m.Size());
-//}
-//Matrix::Matrix(Matrix&& m) noexcept {
-//  (*this) = std::move(m);
-//}
-//
-//double Matrix::M(Elem e) const {
-//  Matrix m = this->Cut(e);
-//  return m.Det();
-//}
-//double Matrix::A(Elem e) const {
-//  auto& [row, col] = e;
-//  return pow(-1, row + col) * (this->M(e));
-//}
-//inline double MultypleDiagonal(
-//  const Data& data, 
-//  size_t begin,
-//  size_t offset,
-//  size_t n
-//  ) 
-//{
-//  double 
-//    res = 0,
-//    mul = 0;
-//  size_t 
-//    row = begin, // для высчитывания эелемента
-//    col = 0;
-//    //fO = n + 1, // смещение главной диагонали
-//    //sO = n - 1; // смещение побочной
-//                  // расчет главной диагонали
-//  for(size_t rStep = 0; rStep < n; ++rStep, row += n) {
-//    mul = data[row];
-//    size_t cStep = 1;
-//    for(col = row + offset; cStep < n; ++cStep, col += offset) {
-//      mul *= data[col % data.size()];
-//    }
-//    res += mul;
-//  }
-//  return res;
-//}
-//double Matrix::Det() const {
-//  Assert(Row() == Col());
-//  if(Row() == 1) return data[0];
-//  else if(Row() == 2) {
-//    return data[0] * data[3] - data[1] * data[2];
-//  }
-//  // master diagonal
-//  double res = MultypleDiagonal(data, 0, Row()+1, Row());
-//  // second
-//  res -= MultypleDiagonal(data, Row()-1, Row()-1, Row());
-//
-//  //double 
-//  //  res = 0, // конечный результат
-//  //  mul = 0; // хранит произведение диагоналей
-//  //size_t 
-//  //  row = 0, // для высчитывания эелемента
-//  //  col = 0,
-//  //  fO = Col()+1, // смещение главной диагонали
-//  //  sO = Col()-1; // смещение побочной
-//  //// расчет главной диагонали
-//  //for(size_t rStep = 0; rStep < Row(); ++rStep, row += Col()) {
-//  //  mul = data[row];
-//  //  size_t cStep = 1;
-//  //  for(col = row + fO; cStep < Row(); ++cStep, col += fO) {
-//  //    mul *= data[col % Size()];
-//  //  }
-//  //  res += mul;
-//  //}
-//  //// расчет побочной диагонали
-//  //row = sO;
-//  //for(size_t rStep = 0; rStep < Row(); ++rStep, row += Col()) {
-//  //  mul = data[row];
-//  //  size_t cStep = 1;
-//  //  for(col = row + sO; cStep < Row(); ++cStep, col += sO) {
-//  //    mul *= data[col % Size()];
-//  //  }
-//  //  res -= mul;
-//  //}
-//  return res;
-//}
-//double Matrix::Det(Elem start) const {
-//  bool Det_NoImplement = false;
-//  Assert(Det_NoImplement);
-//  return 0;
-//}
-//
-//Matrix&& Matrix::operator=(Matrix&& m) noexcept {
-//  Swap(std::move(m));
-//  return move(*this);
-//}
-//
-//Matrix& Matrix::operator=(const Matrix& b) {
-//  this->Clean();
-//  this->Alloc(b.Row(), b.Col());
-//  this->Copy(b.data, b.Size());
-//  return *this;
-//}
-//
-//Matrix& Matrix::operator=(const vector<double>& vec) {
-//  Assert(vec.size() == this->Size());
-//  //memcpy(this->data, vec.data(), vec.size() * sizeof(vec[0]));
-//  data = vec;
-//  return *this;
-//}
-//double& Matrix::operator[](Elem e) {
-//  const auto& [row, col] = e;
-//  assert(row < Row() && col < Col());
-//
-//  return this->data[row * Col() + col];
-//}
-//const double& Matrix::operator[](Elem e) const {
-//  const auto& [row, col] = e;
-//  assert(row < this->Row() && col < this->Col());
-//
-//  return this->data[row * this->Col() + col];
-//}
-//double& Matrix::operator[](size_t e) {
-//  assert(e < this->Size());
-//  return this->data[e];
-//}
-//const double& Matrix::operator[](size_t e) const {
-//  assert(e < this->Size());
-//  return this->data[e];
-//}
-//
-//
-//size_t Matrix::Row() const noexcept { return this->r; }
-//size_t Matrix::Col() const noexcept { return this->c; }
-//size_t Matrix::Size() const noexcept { return Row() * Col(); }
-//
-//Matrix Matrix::operator+(const Matrix& b) const {
-//  assert(this->Row() == b.Row() && this->Col() == b.Col());
-//
-//  Matrix res(b.Row(), b.Col());
-//  Elem e(0,0);
-//  auto& [row, col] = e;
-//  for(row = 0; row < b.Row(); ++row)
-//    for(col = 0; col < b.Col(); ++col) res[e] = (*this)[e] + b[e];
-//
-//  return res;
-//}
-//Matrix Matrix::operator-(const Matrix& b) const {
-//  assert(this->Row() == b.Row() && this->Col() == b.Col());
-//  
-//  Matrix res(b.Row(), b.Col());
-//  Elem e(0,0);
-//  auto& [row, col] = e;
-//  for(row = 0; row < b.Row(); ++row)
-//    for(col = 0; col < b.Col(); ++col) res[e] = (*this)[e] - b[e];
-//
-//  return res;
-//}
-//Matrix Matrix::operator*(const Matrix& b) const {
-//  cout << this->Col() << ' ' << this->Row() << '\n';
-//  assert(this->Col() == b.Row());
-//  
-//  Matrix result(this->Row(), b.Col());
-//
-//  Elem e;
-//  auto& [row, col] = e;
-//
-//  for(row = 0; row < result.Row(); ++row) {
-//    for(col = 0; col < result.Col(); ++col) {
-//      result[e] = 0;
-//      for(size_t index = 0; index < this->Col(); ++index) {
-//        result[e] += ((*this)[{row, index}] * b[{index, col}]);
-//      }
-//    }
-//  }
-//  return result;
-//}
-//Matrix Matrix::operator*(double val) const {    
-//  Matrix res((*this).Row(), (*this).Col());
-//  Elem e(0,0);
-//  auto& [row, col] = e;
-//  for(row = 0; row < (*this).Row(); ++row)
-//    for(col = 0; col < (*this).Col(); ++col) res[e] = (*this)[e] * val;
-//
-//  return res;
-//}
-//Matrix Matrix::operator/(double val) const {    
-//  Matrix res((*this).Row(), (*this).Col());
-//  Elem e(0,0);
-//  auto& [row, col] = e;
-//  for(row = 0; row < (*this).Row(); ++row)
-//    for(col = 0; col < (*this).Col(); ++col) res[e] = (*this)[e] / val;
-//
-//  return res;
-//}
-//
-//Matrix Matrix::Cut(Elem cutRowCol) const {
-//  auto&[skipRow,skipCol]=cutRowCol;
-//  --skipRow;
-//  --skipCol;
-//  assert(skipRow < Row() && skipCol < Col());
-//
-//  Matrix m(this->Row()-1, this->Col()-1);
-//
-//  size_t 
-//    dr = 0,
-//    dc = 0;
-//
-//  for(size_t row=0, dr=0 ; row < Row(); ++row){
-//    if(row==skipRow) continue;
-//    for(size_t col=0, dc = 0; col < Col(); ++col){
-//      if(col==skipCol) continue;
-//      m[{dr, dc}] = (*this)[{row, col}];
-//      ++dc;
-//    }
-//    ++dr;
-//  }
-//  return m;
-//}
-//
-//Matrix::~Matrix() {
-//  this->Clean();
-//}
-//
-//void Matrix::Swap(Matrix&& m) {
-//  //this->data = m.data;
-//  data.swap(m.data);
-//  this->r = m.r;
-//  this->c = m.c;
-//
-//  //m.data = nullptr;
-//  m.r = 0;
-//  m.c = 0;
-//}
-//void Matrix::Copy(const Data& data, size_t count) {
-//  assert(this->Size() <= count);
-//  //for(size_t i = 0; i < count; ++i)
-//  // this->data[i] = data[i];
-//  //memcpy(this->data, data, count * sizeof(double));
-//  this->data = data;
-//}
-//void Matrix::Alloc(size_t r, size_t c) {
-//  this->r = r;
-//  this->c = c;
-//
-//  //this->data = new double[r*c];
-//  data.resize(r * c);
-//}
-//void Matrix::Clean() {
-//  //if(this->data) delete[] this->data;
-//  //this->data = nullptr;
-//  data.clear();
-//  this->r = this->c = 0;
-//}
+#include <cassert>
 
 #include "matrix.h"
 
+namespace Math {
+
+Matrix::Matrix() {
+}
+
+Matrix::Matrix(size_t row, size_t col) {
+  init(row, col); 
+}
+
+Matrix::Matrix(const Matrix& mat) {
+  *this = mat;
+}
+
+Matrix::~Matrix() {
+}
+
+double Matrix::minor(point_type e) const {
+  Matrix m = this->cut(e);
+  return m.determine();
+}
+
+double Matrix::algebraic(point_type e) const {
+  auto& [row, col] = e;
+  return pow(-1, row + col) * (this->minor(e));
+}
+
+static double multypleDiagonal(const typename Matrix::data_type& data, 
+                        size_t begin,
+                        size_t offset,
+                        size_t n)
+{
+  double 
+    res = 0,
+    mul = 0;
+  size_t row = begin,
+          col = 0;
+
+  for(size_t rStep = 0; rStep < n; ++rStep, row += n) {
+    mul = data[row];
+    size_t cStep = 1;
+    for(col = row + offset; cStep < n; ++cStep, col += offset) {
+      mul *= data[col % data.size()];
+    }
+    res += mul;
+  }
+  return res;
+}
+
+double Matrix::determine() const {
+  if (rows() != cols()) {
+    return 0.0;
+  }
+
+  if (rows() == 1) {
+    return _data[0];
+  }
+  else if(rows() == 2) {
+    return _data[0] * _data[3] - _data[1] * _data[2];
+  }
+
+  double res  = multypleDiagonal(_data,          0, rows() + 1, rows());
+          res -= multypleDiagonal(_data, rows() - 1, rows() - 1, rows());
+
+  return res;
+}
+
+Matrix& Matrix::operator=(const data_type& data) {
+  assert(data.size() == _cols * _rows);
+  _data = data;
+  return *this;
+}
+
+Matrix& Matrix::operator=(const Matrix& mat) {
+  init(mat.rows(), mat.cols());
+  _data = mat._data;
+  return *this;
+}
+
+double& Matrix::operator[](point_type e) {
+  const auto& [row, col] = e;
+  assert(row < rows() && col < cols());
+  return _data[row * cols() + col];
+}
+
+const double& Matrix::operator[](point_type e) const {
+  const auto& [row, col] = e;
+  assert(row < rows() && col < cols());
+  return _data[row * cols() + col];
+}
+
+double& Matrix::operator[](size_t e) {
+  assert(e < size());
+  return _data[e];
+}
+
+const double& Matrix::operator[](size_t e) const {
+  assert(e < size());
+  return _data[e];
+}
+
+size_t Matrix::rows() const { 
+  return _rows; 
+}
+
+size_t Matrix::cols() const { 
+  return _cols; 
+}
+
+size_t Matrix::size() const { 
+  return rows() * cols(); 
+}
+
+Matrix& Matrix::operator+=(const Matrix& b) {
+  assert(rows() == b.rows() && cols() == b.cols());
+
+  for (size_t i = 0; i < size(); ++i) {
+    _data[i] += b._data[i];
+  }
+
+  return *this;
+}
+
+Matrix& Matrix::operator-=(const Matrix& b) {
+  assert(rows() == b.rows() && cols() == b.cols());
+  
+  for (size_t i = 0; i < size(); ++i) {
+    _data[i] -= b._data[i];
+  }
+
+  return *this;
+}
+  
+Matrix& Matrix::operator*=(value_type val) {    
+  for (size_t i = 0; i < size(); ++i) {
+    _data[i] *= val;
+  }
+  return *this;
+}
+
+Matrix& Matrix::operator/=(value_type val) {    
+  assert(val != 0);
+  for (size_t i = 0; i < size(); ++i) {
+    _data[i] /= val;
+  }
+  return *this;
+}
+
+Matrix Matrix::cut(point_type cutRowCol) const {
+  auto& [skipRow, skipCol] = cutRowCol;
+
+  assert(skipRow < rows() && skipCol < cols());
+
+  Matrix m(rows() - 1, cols() - 1);
+
+  for(size_t row = 0, dr = 0 ; row < rows(); ++row, ++dr) {
+    if (row == skipRow) {
+      continue;
+    }
+
+    for(size_t col = 0, dc = 0; col < cols(); ++col, ++dc) {
+      if (col == skipCol) {
+        continue;
+      }
+      m[{dr, dc}] = (*this)[{row, col}];
+    }
+  }
+  return m;
+}
+
+void Matrix::swap(Matrix&& m) {
+  _data.swap(m._data);
+  
+  std::swap(_rows, m._rows);
+  std::swap(_cols, m._cols);
+}
+
+void Matrix::init(size_t r, size_t c) {
+  _rows = r;
+  _cols = c;
+
+  _data.resize(size());
+}
+
+void Matrix::clear() {
+  _data.clear();
+  _rows = _cols = 0;
+}
+
+Matrix operator*(const Matrix& a, const Matrix& b) {
+  assert(a.cols() == b.rows());
+  
+  Matrix result(a.rows(), b.cols());
+
+  size_t row = 0, 
+          col = 0;
+
+  for(row = 0; row < result.rows(); ++row) {
+    for(col = 0; col < result.cols(); ++col) {
+      result[{row, col}] = 0;
+      for(size_t index = 0; index < a.cols(); ++index) {
+        result[{row, col}] += a[{row, index}] * b[{index, col}];
+      }
+    }
+  }
+  return result;
+}
+
+Matrix operator*(const Matrix& a, Matrix::value_type val) {
+  Matrix res(a);
+  res *= val;
+  return res;
+}
+
+
+Matrix operator/(const Matrix& a, Matrix::value_type val) {
+  Matrix res(a);
+  res /= val;
+  return res;
+}
+
+Matrix operator+(const Matrix& a, const Matrix& b) {
+  Matrix res(a);
+  res += b;
+  return res;
+}
+
+Matrix operator-(const Matrix& a, const Matrix& b) {
+  Matrix res(a);
+  res -= b;
+  return res;
+}
+}
--- Math/Matrix/matrix.h
+++ Math/Matrix/matrix.h
@@ -1,402 +1,60 @@
-#ifndef Matrix_H
-#define Matrix_H
+#pragma once
 
-#include <memory>
+#include <vector>
 
 namespace Math {
 
-   struct Point {
-      size_t x;
-      size_t y;
-   };
-
-   class zero_divide_exception : std::exception {};
-
-   namespace Tool {
-      template<class T, size_t size>
-         double MultypleDiagonal(const T (&data)[size], size_t begin, size_t offset, size_t n ) {
-            double res = 0
-                 , mul = 0;
-
-            size_t row = begin
-                 , col = 0;
-
-            for(size_t rStep = 0; rStep < n; ++rStep, row += n) {
-               mul = data[row];
-               size_t cStep = 1;
-               for(col = row + offset; cStep < n; ++cStep, col += offset) {
-                  mul *= data[col % size];
-               }
-               res += mul;
-            }
-            return res;
-         }
-   }
-
-
-   template<class T, size_t step = 1>
-      class mat_iterator : public std::random_access_iterator_tag {
-      public:
-         using value_type = T;
-
-         mat_iterator() {}
-         mat_iterator(T* data) noexcept : data(data) {}
-
-         value_type& operator*() { return *data; }
-         auto& operator++() {
-            data += step;
-            return *this;
-         }
-
-         const value_type& operator[](size_t index) const {
-            return *(data + (step * index));
-         }
-
-         auto& operator--() {
-            data -= step;
-            return *this;
-         }
-
-         auto operator+(int i) {
-            mat_iterator res = *this;
-            res.data += (i * step);
-            return res;
-         }
-         auto operator-(int i) {
-            mat_iterator res = *this;
-            res.data -= (i * step);
-            return res;
-         }
-         bool operator==(const mat_iterator& b) const { return data == b.data; }
-         bool operator!=(const mat_iterator& b) const { return data != b.data; }
-      private:
-         value_type* data { nullptr };
-      };
-
-   template<class T, size_t step>
-      class Vector {
-      public:
-         using value_type = T;
-
-         Vector(size_t size) {
-            Resize(size);
-         }
-         Vector(mat_iterator<T, step> begn, size_t size) {
-            beg = begn;
-            Resize(size);
-         }
-
-         auto begin() const { return beg; }
-         auto end()   const { return beg + Size(); }
-
-         auto& operator[](size_t index) { return beg[index]; }
-
-         auto Size() const { return size; }
-         void Resize(size_t now) { size = now; }
-      private:
-         size_t                         size;
-         mat_iterator<value_type, step> beg;
-      };
-   template<class val_t, size_t row, size_t col>
-      class Matrix {
-      public:
-         using value_type = val_t;
-
-      public:
-         using row_iterator = mat_iterator<value_type,   1>;
-         using col_iterator = mat_iterator<value_type, col>;
-         using row_iter_c   = const row_iterator;
-         using col_iter_c   = const col_iterator;
-
-         Matrix() {}
-
-         value_type& operator[](Point elem) { 
-            return data[elem.y * col + elem.x];
-         }
-         const value_type& operator[](Point elem) const { 
-            return data[elem.y * col + elem.x];
-         }
-
-         auto begin() {
-            return mat_iterator( data );
-         }
-         auto end() {
-            return mat_iterator( data + (col * row) );
-         }
-
-         auto beg_row(size_t index) {
-            return row_iterator(data + index * col);
-         }
-         auto beg_col(size_t index) {
-            return col_iterator(data + index);
-         }
-         auto end_row(size_t index) {
-            return row_iterator(data + index * col + col);
-         }
-         auto end_col(size_t index) {
-            return col_iterator(data + index + col * row);
-         }
-
-         auto Row(size_t index) {
-            return Vector<value_type, 1>(beg_row(index), col);
-         }
-         auto Column(size_t index) {
-            return Vector<value_type, col>(beg_col(index), row);
-         }
-
-         template<class _T>
-            Matrix& operator=(const Matrix<_T, row, col>& b) {
-               std::memcpy(data, b.data, sizeof(value_type) * col * row);
-               return *this;
-            }
-
-         template<class _T>
-            Matrix& operator=(const _T(&dat)[row][col]) {
-               for (size_t i = 0; i < row; ++i) {
-                  data[i] = dat[i / col][i % col];
-               }
-               return *this;
-            }
-
-         template<class _T>
-            Matrix& operator=(_T(&&dat)[row][col]) {
-               for (size_t i = 0; i < row; ++i) {
-                  std::swap(data[i], dat[i / col][i % col]);
-               }
-               return *this;
-            }
-
-         template<class _T>
-            Matrix&& operator=(Matrix<_T, row, col>&& m) noexcept {
-               std::swap(data, m.data);
-               return *this;
-            }
-
-         template<class _T>
-            Matrix operator*(const _T& val) const {
-               Matrix res;
-
-               auto res_iter = res  + row*col
-                  , dat_iter = data + row*col;
-
-               while( dat_iter != data ) {
-                  *(--res_iter) = *(--dat_iter) * val;
-               }
-               return res;
-            }
-
-         template<class _T>
-            Matrix operator/(const _T& val)      const {
-               if (val == 0) throw zero_divide_exception;
-
-               Matrix res;
-
-               auto res_iter = res  + row * col
-                  , dat_iter = data + row * col;
-
-               while (dat_iter != data) {
-                  *(--res_iter) = *(--dat_iter) / val;
-               }
-               return res;
-            }
-
-         template<class _T>
-            Matrix operator+(const Matrix<_T, row, col>& mat) const {
-               Matrix res;
-
-               auto res_iter = res  + row * col
-                  , dat_iter = data + row * col
-                  , mat_iter = mat  + row * col;
-
-               while (dat_iter != data) {
-                  *(--res_iter) = *(--dat_iter) + *(--mat_iter);
-               }
-               return res;
-            }
-         template<class _T>
-            Matrix operator-(const Matrix<_T, row, col>& mat) const {
-               Matrix res;
-
-               auto res_iter = res  + row * col
-                  , dat_iter = data + row * col
-                  , mat_iter = mat  + row * col;
-
-               while (dat_iter != data) {
-                  *(--res_iter) = *(--dat_iter) - *(--mat_iter);
-               }
-               return res;
-            }
-
-         template<class _T, size_t _col>
-            Matrix<value_type, _col, col>   operator*(const Matrix<_T, col, _col>& mat) const {
-               Matrix<value_type, _col, col> res;
-
-               for (size_t n = 0; n < _col * col; ++n) {
-                  size_t r = n / col
-                       , c = n % col;
-
-                  res.data[n] = 0;
-
-                  for (size_t i = 0; i < col; ++i) {
-                     res.data[n] += (mat.data[c * col + i] * data[i * col + c]);
-                  }
-               }
-
-               return res;
-            }
-
-
-         Matrix<value_type, row-1, col-1> Remove(Point rc) const {
-            Matrix<value_type, row - 1, col - 1> res;
-
-            size_t size = (row-1) * (col-1);
-
-            for (size_t n = 0; n < size; ++n) {
-               size_t r = n / (col - 1)
-                    , c = n % (col - 1);
-
-               if (r == rc.x || c == rc.y) continue;
-
-               res.data[n] = data[n];
-            }
-
-            return res;
-         }
-
-         auto operator[](size_t column) { return col_iterator(column); }
-         auto operator[](size_t column) const { return col_iter_c(column); }
-
-         template<class Result = double>
-            Result M(Point e) const {
-               return Remove(e).Det();
-            }
-
-         template<class Result = double>
-            Result A(Point e) const {
-               return pow(-1, e.x + e.y) * M(e);
-            }
-
-         template<class Result = double>
-            Result Det() const {
-               double res = 0;
-               
-               res += Tool::MultypleDiagonal(data,     0, row + 1, row);
-               res -= Tool::MultypleDiagonal(data, row-1, row - 1, row);
-
-               return res;
-            }
-
-         constexpr auto Rows() const { return row; }
-         constexpr auto Cols() const { return col; }
-      private:
-         value_type data[row * col];
-      };
-
-      enum class TypeIter {
-         Row, Col
-      };
-
-      
-
-      template<class At, class Bt, size_t stepA, size_t stepB>
-         At operator*(Vector<At, stepA> a, Vector<Bt, stepB> b) {
-            At res = 0;
-
-            for (size_t i = 0; i < a.Size(); ++i) {
-               res += a[i] * static_cast<At>(b[i]);
-            }
-
-            return res;
-         }
-      template<class At, size_t stepA, class Iter>
-         At operator*(Vector<At, stepA> a, Iter it) {
-            At res = 0;
-
-            for (size_t i = 0; i < a.Size(); ++i, ++it) {
-               res += a[i] * static_cast<At>(*it);
-            }
-
-            return res;
-         }
-      template<class At, size_t stepA, class Iter>
-         At operator*(Iter it, Vector<At, stepA> a) {
-            At res = 0;
-
-            for (size_t i = 0; i < a.Size(); ++i, ++it) {
-               res += a[i] * static_cast<At>(*it);
-            }
-
-            return res;
-         }
+class Matrix {
+public:
+  template<class T>
+    using data_type_impl = std::vector<T>;
+
+  using value_type = double;
+  using data_type = data_type_impl<value_type>;
+  using point_type = std::pair<size_t, size_t>;
+
+  Matrix();
+  Matrix(const Matrix& mat);
+  Matrix(size_t row, size_t col);
+  ~Matrix();
+
+  void init(size_t r, size_t c);
+  void clear();
+  double minor(point_type elem) const;
+  double determine() const;
+  double algebraic(point_type elem) const;
+  Matrix cut(point_type cutRowCol) const;
+
+  size_t rows() const;
+  size_t cols() const;
+  size_t size() const;
+
+public:
+  double& operator[](point_type elem);
+  const double& operator[](point_type elem) const;
+
+  double& operator[](size_t index);
+  const double& operator[](size_t index) const;
+
+  Matrix& operator=(const data_type& data);
+  Matrix& operator=(const Matrix& mat);
+  Matrix& operator*=(value_type val);
+  Matrix& operator/=(value_type val);
+  Matrix& operator+=(const Matrix& b);
+  Matrix& operator-=(const Matrix& b);
+
+private:
+  void swap(Matrix&& m);
+
+  size_t _rows = { 0 };
+  size_t _cols = { 0 };
+  data_type _data;
+};
+
+Matrix operator*(const Matrix& a, Matrix::value_type val);
+Matrix operator/(const Matrix& a, Matrix::value_type val);
+Matrix operator+(const Matrix& a, const Matrix& b);
+Matrix operator-(const Matrix& a, const Matrix& b);
+Matrix operator*(const Matrix& a, const Matrix& b);
 
 }
-
-
-//
-//using namespace std;
-//using Elem = std::pair<size_t, size_t>;
-//using Data = std::vector<double>;
-//
-//#define E_DEF(f,s)            \
-//        Elem e((f),(s));      \
-//        auto& [row, col] = e
-//
-//#ifdef _DEBUG
-//#define Assert(x) if(!x) std::cout << #x << '\n';
-//#elif
-//#define Assert(x) assert(x);
-//#endif
-//
-//class Matrix {
-//public:
-//  Matrix ( ) { }
-//
-//  Matrix(size_t row, size_t col) noexcept;
-//  Matrix(const Matrix& m);
-//  Matrix(Matrix&& m) noexcept;
-//
-//  double M(Elem e) const;   // minor
-//  double A(Elem e) const;   // algebraic complement
-//  double Det() const;       // determinant
-//
-//  size_t Row()  const noexcept;
-//  size_t Col()  const noexcept;
-//  size_t Size() const noexcept;
-//
-//  double& operator[](Elem e);
-//  const double& operator[](Elem e) const;
-//
-//  double& operator[](size_t e);
-//  const double& operator[](size_t e) const;
-//
-//  Matrix& operator=(const Matrix& b);
-//  Matrix& operator=(const vector<double>& vec);
-//  Matrix&& operator=(Matrix&& m) noexcept;
-//
-//  Matrix operator*(double val) const;
-//  Matrix operator/(double val) const;
-//  Matrix operator+(const Matrix& b) const;
-//  Matrix operator-(const Matrix& b) const;
-//  Matrix operator*(const Matrix& b) const;
-//
-//  Matrix Cut(Elem cutRowCol) const;
-//
-//  ~Matrix();
-//private:
-//  double Det(Elem start) const;
-//
-//  void Swap(Matrix&& m);
-//  void Copy(const Data& data, size_t count);
-//  void Alloc(size_t r, size_t c);
-//  void Clean();
-//
-//  size_t r{0}; // row
-//  size_t c{0}; // column
-//
-//  Data data;
-//};
-//
-//ostream& operator<<(ostream& os, const Matrix& m);
-//Matrix GetMatrix();
-
-#endif
